ec5e9dcada1ce7cbdd2efd170858ba59d215ecb3
Signal Over Noise <engineering@signalovernoise.io>
2025-06-20 14:16:05 +0500

    add ai course builder

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,7 @@ import { SubscriptionProvider } from "@/contexts/SubscriptionContext";
 import { HelmetProvider } from "react-helmet-async";
 //import WhatsAppButton from "./components/home/WhatsAppButton";
 import TawktoWidget from "./components/Tawkto";
+import AiCourseBuilder from "./pages/AiCourseBuilder";
 
 function App() {
   const location = useLocation();
@@ -72,83 +73,90 @@ function App() {
   );
   return (
     <>
-    <HelmetProvider>
-      <ThemeProvider attribute="class" defaultTheme="light">
-        <LanguageProvider>
-          <AuthProvider>
-            <SubscriptionProvider>
-              {/* <Router> */}
-              <Analytics />
-              <Routes>
-                {/* Public routes */}
-                <Route path="/" element={<Index />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/blog/:slug" element={<BlogPost />} />
-                <Route path="/pricing" element={<Pricing />} />
-                <Route path="/events" element={<Events />} />
-                <Route path="/terms" element={<TermsOfService />} />
-                <Route path="/privacy" element={<Privacy />} />
-                <Route path="/how-it-works" element={<HowItWorks />} />
+      <HelmetProvider>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <LanguageProvider>
+            <AuthProvider>
+              <SubscriptionProvider>
+                {/* <Router> */}
+                <Analytics />
+                <Routes>
+                  {/* Public routes */}
+                  <Route path="/" element={<Index />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/sign-in" element={<SignIn />} />
+                  <Route path="/sign-up" element={<SignUp />} />
+                  <Route path="/reset-password" element={<ResetPassword />} />
+                  <Route path="/blog" element={<Blog />} />
+                  <Route path="/blog/:slug" element={<BlogPost />} />
+                  <Route path="/pricing" element={<Pricing />} />
+                  <Route path="/events" element={<Events />} />
+                  <Route path="/terms" element={<TermsOfService />} />
+                  <Route path="/privacy" element={<Privacy />} />
+                  <Route path="/how-it-works" element={<HowItWorks />} />
 
-                {/* Discipline-specific routes */}
-                <Route path="/dressage" element={<Dressage />} />
-                <Route path="/jumping" element={<Jumping />} />
-                <Route
-                  path="/dressage/how-it-works"
-                  element={<DressageHowItWorks />}
-                />
-                <Route
-                  path="/jumping/how-it-works"
-                  element={<JumpingHowItWorks />}
-                />
-                <Route path="/equestrian/about" element={<EquestrianAbout />} />
-                <Route path="/dressage/about" element={<DressageAbout />} />
-                <Route path="/jumping/about" element={<JumpingAbout />} />
-
-                {/* Protected user routes */}
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/profile-setup" element={<Profile />} />
-
-                <Route
-                  path="/profile-questionnaire"
-                  element={<ProfileQuestionnaire />}
-                />
-                <Route
-                  path="/jump-profile-setup"
-                  element={<JumpProfileSetup />}
-                />
-                <Route path="/analysis" element={<Analysis />} />
-
-                {/* Admin routes */}
-                <Route path="/admin" element={<Admin />}>
+                  {/* Discipline-specific routes */}
+                  <Route path="/dressage" element={<Dressage />} />
+                  <Route path="/jumping" element={<Jumping />} />
                   <Route
-                    index
-                    element={<Navigate to="/admin/dashboard" replace />}
+                    path="/dressage/how-it-works"
+                    element={<DressageHowItWorks />}
                   />
-                  <Route path="dashboard" element={<AdminDashboard />} />
-                  <Route path="blog" element={<AdminBlog />} />
-                  <Route path="users" element={<AdminUsers />} />
-                  <Route path="content" element={<AdminContent />} />
-                  <Route path="media" element={<AdminMedia />} />
-                  <Route path="settings" element={<AdminSettings />} />
-                </Route>
+                  <Route
+                    path="/jumping/how-it-works"
+                    element={<JumpingHowItWorks />}
+                  />
+                  <Route
+                    path="/equestrian/about"
+                    element={<EquestrianAbout />}
+                  />
+                  <Route path="/dressage/about" element={<DressageAbout />} />
+                  <Route path="/jumping/about" element={<JumpingAbout />} />
 
-                {/* 404 page */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-              {isPublicPage && <TawktoWidget/>}
-              <Toaster />
-              <SonnerToaster />
-              {/* </Router> */}
-            </SubscriptionProvider>
-          </AuthProvider>
-        </LanguageProvider>
-      </ThemeProvider>
-    </HelmetProvider>
+                  {/* Protected user routes */}
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/profile-setup" element={<Profile />} />
+
+                  <Route
+                    path="/profile-questionnaire"
+                    element={<ProfileQuestionnaire />}
+                  />
+                  <Route
+                    path="/jump-profile-setup"
+                    element={<JumpProfileSetup />}
+                  />
+                  <Route path="/analysis" element={<Analysis />} />
+                  <Route
+                    path="/ai-course-builder"
+                    element={<AiCourseBuilder />}
+                  />
+
+                  {/* Admin routes */}
+                  <Route path="/admin" element={<Admin />}>
+                    <Route
+                      index
+                      element={<Navigate to="/admin/dashboard" replace />}
+                    />
+                    <Route path="dashboard" element={<AdminDashboard />} />
+                    <Route path="blog" element={<AdminBlog />} />
+                    <Route path="users" element={<AdminUsers />} />
+                    <Route path="content" element={<AdminContent />} />
+                    <Route path="media" element={<AdminMedia />} />
+                    <Route path="settings" element={<AdminSettings />} />
+                  </Route>
+
+                  {/* 404 page */}
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+                {isPublicPage && <TawktoWidget />}
+                <Toaster />
+                <SonnerToaster />
+                {/* </Router> */}
+              </SubscriptionProvider>
+            </AuthProvider>
+          </LanguageProvider>
+        </ThemeProvider>
+      </HelmetProvider>
     </>
   );
 }
